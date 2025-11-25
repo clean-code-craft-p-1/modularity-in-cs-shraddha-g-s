271ac0feb5aaef5658dd0850dbf45b6ba0389783
shraddha-g-s <shraddha.gs@philips.com>
2025-11-25 23:24:03 +0530

    Removed unnecessary imports

--- BatchProcessor.cs
+++ BatchProcessor.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TemperatureAnalysis
+﻿namespace TemperatureAnalysis
 {
     public static class BatchProcessor
     {
